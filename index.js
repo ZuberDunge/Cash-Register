40dfae8d0596f1dac7b021cfcde35d6fdfff4036
ZuberDunge <iamZuberDunge@gmail.com>
2021-09-13 11:01:09 +0530

    next button added

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const checkButton = document.querySelector("#check-btn");
 const message = document.querySelector("#error-msg");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const nextButton = document.querySelector("#next-btn");
+const  afterNext = document.querySelector("#after-next"); 
+const tableSection = document.querySelector(".table-section");
+
+
+nextButton.addEventListener("click", ()=>{
+if(Number(billAmount.value)>=1){
+    afterNext.style.display=`block`;
+}else{
+
+
+showMessage("Enter a valid amount ");
+}
+});
 
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
@@ -17,9 +31,15 @@ let billAmountValue = Number(billAmount.value);
                 if (cashGivenValue >= billAmountValue) {
                     const amountToBeReturned = cashGivenValue - billAmountValue;
                     calculateChange(amountToBeReturned);
+                    tableSection.style.display = `block`;
                 } else {
                     showMessage("do you wanna wash the plates?");
                 }
+
+
+                
+
+
 
     } else {
         showMessage("Entered amount is invalid");
@@ -31,6 +51,7 @@ function calculateChange(amountToBeReturned) {
     for (let i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc( amountToBeReturned / availableNotes[i]
         );
+        tableSection.style.display = `block`;
         amountToBeReturned %= availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
